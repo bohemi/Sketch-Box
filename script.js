09858fea98c676c1c6ee6d5c67466de8ab04b90d
bohemi <ankitsingh5940@gmail.com>
2025-08-07 17:37:54 +0530

    Drawing on div element will perform when holding down click button. Fixed box container positining with margin. Made drawing div boxed to stretch evenly on the container.

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const parentBox = document.querySelector("#sketchBoxContainer");
 
-createGrid(parentBox,"blue", 30);
+createGrid(parentBox, "lightpink", 196); // 196 to be minimum and 14 takes each row
+
+const colorBoxes = document.querySelectorAll(".colorBoxes"); // let the grid be created then get getting the boxes.
+
+drawOnBoxes(colorBoxes);
+
+function drawOnBoxes(box) {
+  let isMouseButtonHeldDown = false; // initially false so that it doesn't draw on boxes when the mouse is not pressed.
+  colorBoxes.forEach((box) => {
+    box.onmouseenter = function (event) {
+      if (isMouseButtonHeldDown == true) {
+        box.style.backgroundColor = "blue";
+      }
+    };
+    // will draw on boxes when the mouse is pressed and moved over them.
+    box.onmousedown = function (event) {
+      isMouseButtonHeldDown = true;
+    };
+    // would not draw on boxes when the mouse button is released.
+    box.onmouseup = function (event) {
+      isMouseButtonHeldDown = false;
+    };
+  });
+}
 
 function createGrid(parent, color, gridLength) {
   for (let i = 0; i < gridLength; i++) {
-    const div = document.createElement("div");
-    div.setAttribute("class", "colorBoxes");
-
-    if ((i + 1) % 2 === 0) {
-      div.style.backgroundColor = color;
-    }
-    parent.appendChild(div);
+    const colorBoxDivs = document.createElement("div");
+    colorBoxDivs.setAttribute("class", "colorBoxes");
+    parent.appendChild(colorBoxDivs);
   }
 }
